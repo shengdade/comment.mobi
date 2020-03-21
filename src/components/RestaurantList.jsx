05179676d8d2524fb26d79a934cdf614d812a829
Dade Sheng <dasheng@amazon.com>
2020-03-21 14:03:54 -0400

    list restaurants

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listRestaurants } from '../graphql/queries';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const fetchList = async setLoaded => {
+  const posts = await API.graphql(
+    graphqlOperation(listRestaurants, { limit: 100 })
+  );
+  console.log(posts);
+  setLoaded(true);
+};
 
 const RestaurantList = () => {
-  return <div>RestaurantList</div>;
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    fetchList(setLoaded);
+  }, []);
+  return (
+    <>
+      {!loaded && <LinearProgress />}
+      <div>content</div>
+    </>
+  );
 };
 
 export default RestaurantList;
