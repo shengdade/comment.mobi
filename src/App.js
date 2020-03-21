50f3aed7d6c6f19570a095d076502ac81e82bc0f
Dade Sheng <dasheng@amazon.com>
2020-03-20 23:49:01 -0400

    add to users group post registration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 function App() {
+  const [isUser, updateUserInfo] = useState(false);
+  useEffect(() => {
+    /* Get the AWS credentials for the current user from Identity Pools.  */
+    Auth.currentSession()
+      .then(cognitoUser => {
+        const {
+          idToken: { payload }
+        } = cognitoUser;
+        /* Loop through the groups that the user is a member of */
+        /* Set isUser to true if the user is part of the USERS group */
+        payload['cognito:groups'] &&
+          payload['cognito:groups'].forEach(group => {
+            if (group === 'users') updateUserInfo(true);
+          });
+      })
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isUser && <h1>Welcome, User!</h1>}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
