0dd7c63270ce39ae9be86fc1febd9808c0dc55d9
Dade Sheng <dasheng@amazon.com>
2020-03-21 10:25:06 -0400

    sign in/up

--- src/App.js
+++ src/App.js
@@ -9,31 +9,35 @@ import config from './aws-exports';
 Amplify.configure(config);
 
 function App() {
-  const [isUser, updateUserInfo] = useState(false);
+  const [cognitoGroup, updateCognitoGroup] = useState('');
   useEffect(() => {
-    /* Get the AWS credentials for the current user from Identity Pools.  */
     Auth.currentSession()
       .then(cognitoUser => {
         const {
           idToken: { payload }
         } = cognitoUser;
-        /* Loop through the groups that the user is a member of */
-        /* Set isUser to true if the user is part of the USERS group */
-        payload['cognito:groups'] &&
-          payload['cognito:groups'].forEach(group => {
-            if (group === 'users') updateUserInfo(true);
-          });
+        const groups = payload['cognito:groups'];
+        if (groups) {
+          if (groups.includes('admin')) updateCognitoGroup('admin');
+          else if (groups.includes('owners')) updateCognitoGroup('owners');
+          else if (groups.includes('users')) updateCognitoGroup('users');
+        }
       })
       .catch(err => console.log(err));
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {isUser && <h1>Welcome, User!</h1>}
+        <h1>{`Welcome, ${cognitoGroup}!`}</h1>
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App, { includeGreetings: true });
+export default withAuthenticator(App, {
+  includeGreetings: true,
+  usernameAttributes: 'email',
+  signUpConfig: { hiddenDefaults: ['phone_number'] }
+});
